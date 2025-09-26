92265359824ec239d5a440c966157d5644427fa4
Ghost <ghost@github.com>
2025-09-26 17:05:00 +0200

    Added the major refactor python code, excluding the hidden files

--- src/__main__.py
+++ src/__main__.py
@@ -6,9 +6,15 @@ import typing as t
 import requests
 import unzip_http  # type: ignore
 import yaml
+import json
+from pathlib import Path
+from html.parser import HTMLParser
 from loguru import logger
 
-from src import models
+GITHUB_ACTION = os.getenv("GITHUB_ACTION")
+
+if GITHUB_ACTION != "true":
+    from src import models
 
 SENTRY_DSN = os.getenv("SENTRY_DSN")
 if SENTRY_DSN:
@@ -28,6 +34,40 @@ TIMEOUT = 5
 BACKFILL_DAYS = 7
 
 RoundSummary = t.Dict[str, t.Any]
+
+
+
+# OLD BACKUP is just a safe guard against me doing something dumb, and replacing the old links with current
+# OLD is a list of zip links that were collected and processed, basically all the stuff that is in the DB already
+# PREVIOUS is a list of zip links that got collected in the previous CI run, did get downloaded, but were not added into the DB
+# CURRENT is a list ALL zip links that got collected right now
+# NEW is a difference between PREVIOUS and CURRENT, ie. links that are new, and were not even downloaded
+OLD_REPLAY_LINKS_BACKUP = "oldReplayLinksBackup.json"
+OLD_REPLAY_LINKS = "oldReplayLinks.json"
+PREVIOUS_REPLAY_LINKS = "previousReplayLinks.json"
+
+CURRENT_REPLAY_LINKS = "currentReplayLinks.json" # technically never used just keeping it as a documentation
+NEW_REPLAY_LINKS = "newReplayLinks.json" # technically never used just keeping it as a documentation
+
+# CURRENT - OLD = all the files that were not added to the DB yet, basically all the files in `data/yaml` folder
+# CURRENT - PREVIOUS = all the files that i am about to download, that got created since the previous run
+
+
+CURRENT_FILE_PARENT = Path(Path(__file__).parent)
+IN_DOCKER = os.getenv("IN_DOCKER")
+
+
+class LinkHTMLParser(HTMLParser):
+
+    def __init__(self):
+        super().__init__()
+        self.crawlerPaths = []
+
+    # Find every anchor tag in the current HTML page
+    def handle_starttag(self, tag, attributes):
+        if tag == "a" and attributes[0][0] == "href":
+            self.crawlerPaths.append(attributes[0][1])
+
 
 
 class ExtractOutput(t.TypedDict):
@@ -107,6 +147,36 @@ class Replays:
                 yield {"date": date, "data": data}
             except Exception:
                 logger.warning(f"Bad zip file {replay_url}")
+
+
+    # Same as _extract, but goes through every file in src/data/yaml, and reads the them
+    def _extractFromLocal(self) -> t.Iterator[ExtractOutput]:
+        for localFilePath in Path( CURRENT_FILE_PARENT / "data" / "yaml/").iterdir():
+            year, month, day = localFilePath.name.split("-")[0].split("_")
+            hour, minute = localFilePath.name.split("-")[1].split("_")
+            date = datetime.datetime(
+                year=int(year),
+                month=int(month),
+                day=int(day),
+                hour=int(hour),
+                minute=int(minute),
+            )
+            try:
+                with localFilePath.open("r") as f:
+                    logger.info(f"Loading from file {localFilePath}")
+                    data = yaml.safe_load(f)
+
+                    if data is None:
+                        logger.warning(f"Missing summary data {localFilePath}")
+                        continue
+                    if data["roundEndText"] is None:
+                        logger.warning(f"Missing round end text {localFilePath}")
+                        continue
+
+                    yield {"date": date, "data": data}
+            except Exception as e:
+                logger.warning(f"{e} == at file == {localFilePath}")
+
 
     def _transform(
         self,
@@ -299,5 +369,116 @@ class Replays:
             self._load(transform_output)
 
 
-replays = Replays()
-replays.process()
+    def saveIntoJSONFile(self, data, filename):
+        with Path(CURRENT_FILE_PARENT / "data" / filename).open("w") as f:
+            json.dump(data, f, indent=4)
+
+    def loadFromJSONFile(self, filename):
+        with Path(CURRENT_FILE_PARENT / "data" / filename).open("r") as f:
+            return json.load(f)
+
+    def collectCurrentReplayLinks(self, url):
+
+        print("Current URL=" + url)
+        parser = LinkHTMLParser()
+        
+        response = requests.get(url, timeout=5)
+        parser.feed(response.text)
+
+        for path in parser.crawlerPaths:
+            if path.startswith(".."):
+                continue
+            if path.endswith("/"):
+                self.collectCurrentReplayLinks(url + path)
+            if path.endswith(".zip"):
+                self.currentReplayLinks.append(url + path)
+        return
+
+
+    def findNewReplayLinks(self):
+        self.previousReplayLinks = self.loadFromJSONFile(PREVIOUS_REPLAY_LINKS)
+        self.collectCurrentReplayLinks(ROOT_URL)
+        # Get only links that are new since the last time
+        # Basically a set(currentLinks) - set(previousLinks), but sets dont keep the nice list ordering,
+        # not that it should matter that much
+        self.newReplayLinks = [link for link in self.currentReplayLinks if link not in self.previousReplayLinks]
+
+
+    def downloadFile(self, replay_url):
+        zipFileName = Path(replay_url).name
+        localFilePath = Path( CURRENT_FILE_PARENT / "data" / "yaml" / zipFileName).with_suffix(".yaml")
+
+        try:
+            remote_zip_file = unzip_http.RemoteZipFile(replay_url)
+            yamlData = remote_zip_file.open("_replay/replay_final.yml").read()
+            with localFilePath.open("wb") as f:
+                f.write(yamlData)
+
+        except Exception:
+            print(f"Ingoring a bad zip file {replay_url}")
+
+
+    # Used only in the case that you dont have the oldReplayLinks file so you are starting completely fresh
+    def collectEverythingWhenNoData(self):
+        self.collectCurrentReplayLinks(ROOT_URL)
+        self.saveIntoJSONFile(self.currentReplayLinks, OLD_REPLAY_LINKS)
+
+    # Given a list of links just download them all
+    def downloadReplaysFromFile(self, filename):
+        replayList = self.loadFromJSONFile(filename)
+        for link in replayList:
+            self.downloadFile(link)
+
+
+
+
+
+
+    def processFromLocalFiles(self):
+
+
+
+        # Get links when you have no previous data, and then download them
+        # self.collectEverythingWhenNoData()
+        # self.downloadReplaysFromFile(OLD_REPLAY_LINKS)
+
+        # Should be ran only when triggered in CICD
+        if GITHUB_ACTION == "true":
+            self.findNewReplayLinks()
+            for replayLink in self.newReplayLinks:
+                self.downloadFile(replayLink)
+            # current becomes previous
+            self.saveIntoJSONFile(self.currentReplayLinks, PREVIOUS_REPLAY_LINKS)
+        elif IN_DOCKER == "true":
+            for extract_output in self._extractFromLocal():
+                transform_output = self._transform(extract_output)
+                if not transform_output:
+                    continue
+                self._load(transform_output)
+        else:
+            print("Didnt do anything")
+
+
+
+
+if __name__ == "__main__":
+    replays = Replays()
+    
+    # Default functionality that can still be triggered, I just replaced it with local file loading
+    # replays.process()
+
+    replays.processFromLocalFiles()
+    print("Done")
+
+
+
+
+
+
+# Bad zip file https://replays.rouny-ss14.com/replays/alamo/2024/06/09/2024_06_09-17_28-round_7.zip
+# Bad zip file https://replays.rouny-ss14.com/replays/alamo/2024/06/09/2024_06_09-18_06-round_74.zip
+# Bad zip file https://replays.rouny-ss14.com/replays/alamo/2024/07/03/2024_07_03-23_06-round_209.zip
+# Bad zip file https://replays.rouny-ss14.com/replays/alamo/2024/07/10/2024_07_10-02_51-round_302.zip
+# Bad zip file https://replays.rouny-ss14.com/replays/alamo/2024/07/10/2024_07_10-22_05-round_315.zip
+# Bad zip file https://replays.rouny-ss14.com/replays/alamo/2024/10/27/2024_10_27-21_33-round_1746.zip
+# Bad zip file https://replays.rouny-ss14.com/replays/alamo/2024/10/28/2024_10_28-20_26-round_1764.zip
